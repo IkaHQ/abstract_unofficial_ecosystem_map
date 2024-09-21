63b8c0d2514ca462c51996478b5c572b235d31b9
IkaHQ <ikahq@outlook.fr>
2024-09-21 10:15:45 +0200

    Update gamings.ts

--- src/protocols/gamings.ts
+++ src/protocols/gamings.ts
@@ -1,6 +1,13 @@
 import { Protocol } from './index';
 
 const gamings: Protocol[] = [
+ {
+    name: 'The Plooshies',
+    url: 'https://theplooshies.com/',
+    logoPath: '/logos/plooshies.png',
+    category: Category.Gaming,
+    twitterUrl: 'https://x.com/ThePlooshies',
+  },
 ];
 
 export { gamings };
